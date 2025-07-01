9a60526470df6f850387bb71bf584aadb139ee91
lyssadev <toolsplus1000@gmail.com>
2025-07-01 22:03:08 +0000

    Fix CORS

--- Testcoin/api.py
+++ Testcoin/api.py
@@ -41,9 +41,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://*.vercel.app"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_origins=["*"],  # Allow all origins for now
+    allow_credentials=False,  # Set to False when using allow_origins=["*"]
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
